3f4787b4e081c434702017b13d2c0c182b97c4dc
LeCOdeXx <iuriroberto123@Gmail.com>
2022-10-10 13:16:23 -0300

    While nos metodos

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,43 +8,89 @@ import entidades.Conta;
 import model.Exception.FullException;
 
 public class Program {
+	public static Integer numero;
+	public static String titular;
+	public static Double saldo;
+	public static Double limite;
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		
+
+				/////////1
 		int x = 1;
+				do {
+					try {
+						System.out.println("Entre com os dados da conta");
+						System.out.print("Numero: ");
+						numero = sc.nextInt();
+						x = 0;
+					} catch (InputMismatchException e) {
+						sc.nextLine();
+						System.out.println("Valor inserido não é valido!!");
+					}
+				} while (x != 0);
+				//////////1
+				
+				
+				/////////2
 
-		do {
-
-			try {
-
-				System.out.println("Entre com os dados da conta");
-				System.out.print("Numero: ");
-				Integer numero = sc.nextInt();
-				System.out.print("Titular: ");
-				sc.nextLine();
-				String titular = sc.nextLine();
-				System.out.print("Saldo inicial: ");
-				Double saldo = sc.nextDouble();
-				System.out.print("Limite de saque: ");
-				Double limite = sc.nextDouble();
+						System.out.print("Titular: ");
+						sc.nextLine();
+						titular = sc.nextLine();
+				
+				/////////2
+				
+				/////////3
+			int y = 1;
+				do {
+					try {
+						System.out.print("Saldo inicial: ");
+						saldo = sc.nextDouble();
+						y = 0;
+					} catch (InputMismatchException e) {
+						sc.nextLine();
+						System.out.println("Valor inserido não é valido");
+					}
+				} while (y != 0);
+				//////////3
+				
+				/////////4
+			int z = 1;
+					do {
+						try {
+							System.out.print("Limite de saque: ");
+							limite = sc.nextDouble();
+							z = 0;
+						} catch (InputMismatchException e) {
+							sc.nextLine();
+							System.out.println("Valor inserido não é valido!!");
+						}
+					} while (z != 0);
+				/////////4
 
 				Conta conta = new Conta(numero, titular, saldo, limite);
+			int a = 1;
+				do {
+					try {
+						System.out.println();
+						System.out.print("Entre com a quantidade para saque: ");
+						Double quantia = sc.nextDouble();
+						conta.saque(quantia);
+						a = 0;
+						System.out.println();
+						System.out.println(conta.toString());
+					} catch (InputMismatchException e) {
+						sc.nextLine();
+						System.out.println("Valor inserido não é valido!!");
+					} catch (FullException e) {
+						System.out.println("Erro: " + e.getMessage());
+					}
+				} while (a != 0);
+			
+			
 
-				System.out.println();
-				System.out.print("Entre com a quantidade para saque: ");
-				Double quantia = sc.nextDouble();
-				conta.saque(quantia);
-				x = 0;
-				System.out.println();
-				System.out.println(conta.toString());
-			} catch (FullException e) {
-				System.out.println("Erro: " + e.getMessage());
-			} catch (InputMismatchException e) {
-				System.out.println("Valor inserido não é valido!!");
-				sc.nextLine();
-			}
-		} while (x == 1);
 		{
 
 			sc.close();
